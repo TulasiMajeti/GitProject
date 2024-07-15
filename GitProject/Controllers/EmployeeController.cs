5f42b1f758f01ccdd7d6d32993f3a225fc6326ab
Tulasi Lakshmi Majeti <tulasilakshmi.majeti@gmail.com>
2024-07-15 18:01:58 +0530

    Committed controller changes

--- GitProject/Controllers/EmployeeController.cs
+++ GitProject/Controllers/EmployeeController.cs
@@ -9,6 +9,7 @@ namespace GitProject.Controllers
     public class EmployeeController : Controller
     {
         // GET: Employee
+        //Added new controller
         public ActionResult Index()
         {
             return View();
